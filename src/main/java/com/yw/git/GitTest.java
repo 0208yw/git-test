e014f26a406c38aa2a83fea5d870a57d161fa4db
yw <2283092654@qq.com>
2022-06-18 08:48:52 +0800

    first commit

---
+++ src/main/java/com/yw/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.yw.git;
+
+/**
+ * @author 杨武
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
